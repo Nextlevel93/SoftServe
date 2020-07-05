fdc027e339f81ef5856966857d4678eada8310f1
nlevel <nextlevel1993@gmail.com>
2020-07-05 23:47:26 +0300

    push

--- HomeWork3/Task4/Task3.4.js
+++ HomeWork3/Task4/Task3.4.js
@@ -1,3 +1,10 @@
 let mass = "is2 Thi1s T4est 3a";
-mass.split([" "]);
-console.log(mass);  
+let i =mass.split(" ")
+//console.log(i);  
+let new_Mass;
+//console.log(i.length); 
+for( let x=0; x<i.length; x++){
+    var r =  new RegExp("\d+");
+    i[x].match(r);
+    console.log(i[x], r);
+}
