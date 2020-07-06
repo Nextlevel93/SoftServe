1ebd0dc59a20cd7cd33d44063931b2587cd88d63
Nextlevel93 <41473615+Nextlevel93@users.noreply.github.com>
2020-07-06 16:24:56 +0300

    Update Task3.4.js

--- HomeWork3/Task4/Task3.4.js
+++ HomeWork3/Task4/Task3.4.js
@@ -1,11 +1,15 @@
-let regexp = /\d/;
-let mass = "4of Fo1r pe6ople g3ood th5e the2";
-let i = mass.split(" ");
-let new_Mass = [];
-let x;
-for( let y = 0; y < i.length; y++){
+function correctLine(){
+   let regexp = /\d/;
+  let i = mass.split(" ");
+  let new_Mass = [];
+  let x;
+  for( let y = 0; y < i.length; y++){
    let x = i[y].match(regexp);
    new_Mass[x] = i[y];
   }
-new_Mass.shift();
-console.log(new_Mass);
+  new_Mass.shift();
+  console.log(new_Mass);
+  }
+
+let mass = "4of Fo1r pe6ople g3ood th5e the2";
+correctLine(mass);
