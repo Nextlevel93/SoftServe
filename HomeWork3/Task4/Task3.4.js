6803c8d1d36189715786619f6f3ec76d9974b2ef
Nextlevel93 <41473615+Nextlevel93@users.noreply.github.com>
2020-07-06 16:14:25 +0300

    Update Task3.4.js

--- HomeWork3/Task4/Task3.4.js
+++ HomeWork3/Task4/Task3.4.js
@@ -1,10 +1,10 @@
+let regexp = /\d/;
 let mass = "is2 Thi1s T4est 3a";
-let i =mass.split(" ")
-//console.log(i);  
-let new_Mass;
-//console.log(i.length); 
-for( let x=0; x<i.length; x++){
-    var r =  new RegExp("\d+");
-    i[x].match(r);
-    console.log(i[x], r);
-}
+let i = mass.split(" ");
+let new_Mass = [];
+let x;
+for( let y = 0; y < i.length; y++){
+   let x = i[y].match(regexp);
+   new_Mass[x] = i[y];
+  }
+console.log(new_Mass);
