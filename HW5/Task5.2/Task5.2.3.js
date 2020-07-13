79e03f3b7d5c1ad3626a1fb8b7ec04787634908e
nlevel <nextlevel1993@gmail.com>
2020-07-13 17:53:40 +0300

    3.3notdone

---
+++ HW5/Task5.2/Task5.2.3.js
@@ -0,0 +1,17 @@
+const  str = "I`m 27 yoers Old";
+let search_Numbers = /\d/;
+let serach_Letters = /\w/;
+
+function A(){
+  let new_str = [];
+  let letters=0;
+  let symbols=0;
+    for (let i=0; i<str.length; i++){
+      str[i].match(search_Numbers)?new_str.push("_"):
+      str[i].toUpperCase()?new_str.push(`str[i].toLowerCase()`):
+      str[i].toLowerCase()?new_str.push(`str[i].toUpperCase()`):0
+      ;
+    }  
+  return (`${new_str}`);
+}
+console.log(A(str));
