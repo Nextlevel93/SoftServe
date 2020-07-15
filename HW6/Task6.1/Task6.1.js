3f6bb1ca84293d34558bd9c52f6f905c1190aa0c
nlevel <nextlevel1993@gmail.com>
2020-07-15 17:51:01 +0300

    6.1

--- HW6/Task6.1/Task6.1.js
+++ HW6/Task6.1/Task6.1.js
@@ -1,23 +1,52 @@
-let DNA = "ATTDGC";
+let result = [];
+let firsttd = div1.getElementsByTagName('td');
 
-function reject (){
-  let new_DNA=[];
-  let i=0;
-  for(i; i<DNA.split("").length; i++){
-    if(DNA[i]==="A"){
-       new_DNA.push("T");
-    }
-    else if(DNA[i]==="T"){
-       new_DNA.push("A");
-    }
-     else if(DNA[i]==="C"){
-      new_DNA.push("G");
-    }
-     else if(DNA[i]==="G"){
-       new_DNA.push("C");
-    }
+function getListContent() {
+    for(let i=1; i<=10; i++) {
+    let div1 = document.createElement('tr');
+    div1.append();
+    result.push(div1);
+    for(let p=1; p<=10; p++){
+    let div1 = document.createElement('td');
+    div1.append();
+     result.push(div1);      
   }
-   return new_DNA;
+  }
+  
+  return result;
+  
+}
+div1.append(...getListContent());
+
+function getRandomColor() {
+   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+} 
+
+const arr = [];
+for(let start=1;start<=100;start++){
+  arr.push(start);
+}
+function getrandomcell2(){
+  let final=arr;
+  let length =final.length;
+  let  randomNum = Math.floor(Math.random() * length);
+  let i =final.indexOf(randomNum);
+  final.splice(i,1);
+    console.log(final[randomNum],i,final.length);
+   return final[randomNum];
 }
 
-console.log(reject(DNA));
+
+//console.log(getrandomcell2());
+
+/*function getRandomcell() {
+  return Math.floor(Math.random() *25);;
+}*/
+
+setInterval(function changeColor () {
+       firsttd[getrandomcell2()].style.background = getRandomColor(); 
+    }
+,1000);
+  
+  
+
--- HomeWork1/Task6/Task6.js
+++ HomeWork1/Task6/Task6.js
@@ -1,7 +1,10 @@
-let num = prompt('Введите количество валюты для конвертации');
-const usd = 26.69;
-const eur= 29.95;
-const coef = usd/eur;
-let sum= Math.floor(num*coef*100)/100;
-alert(`Вы получите ${sum} eur`);
+tr.before('before'); // вставить строку "before" перед <ol>
+tr.after('after'); // вставить строку "after" после <ol>
 
+let liFirst = document.createElement('li');
+liFirst.innerHTML = 'prepend';
+ol.prepend(liFirst); // вставить liFirst в начало <ol>
+
+let liLast = document.createElement('li');
+liLast.innerHTML = 'append';
+ol.append(liLast); // вставить liLast в конец <ol>
